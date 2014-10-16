e3ceb4620ac518b832bfd6e2b889304b835f1a7b
plozhkin <plozhkin@avinfors.ru>
2014-10-16 19:35:44 +0400

    добавил все страницы

---
+++ engine.js
@@ -0,0 +1,10 @@
+var app = angular.module('app', [
+    'appCtrl'
+]);
+
+var appCtrl = angular.module('appCtrl', []);
+appCtrl.controller('main', [ '$scope', function($scope) {
+    $scope.register = function() {
+
+    }
+}]);
